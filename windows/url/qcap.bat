1f8002d9b31799ea7d68351d48fcec76f28735e7
Kidd Dawny Lu <kidd.dawny.lu@gmail.com>
2016-07-13 14:49:20 +0800

    update
    Signed-off-by: Kidd Dawny Lu <kidd.dawny.lu@gmail.com>

---
+++ windows/url/qcap.bat
@@ -0,0 +1,3 @@
+@echo off  
+
+start D:\MyFirefox\MyFirefox.exe https://cap.qti.qualcomm.com/default.aspx
---
+++ windows/wrapper/qpst.bat
@@ -0,0 +1,3 @@
+@echo off  
+
+start "" "C:\Program Files (x86)\Qualcomm\QPST\bin\QPSTConfig.exe"
