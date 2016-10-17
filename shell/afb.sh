6583e947f0dc81e4c9ad4f32c388241d4efb4571
huangxiaolu <huangxiaolu@smartisan.com>
2016-10-17 14:06:03 +0800

    update
    Signed-off-by: huangxiaolu <huangxiaolu@smartisan.com>

--- shell/afb.sh
+++ shell/afb.sh
@@ -17,26 +17,25 @@ EOF
 
 ####################################### main ################################################
 
-cpus=$( cat /proc/cpuinfo | grep '^processor'| wc -l) #default
-cpus=$((cpus * 2 - 1))
+cpus=$( grep '^processor' /proc/cpuinfo | wc -l) #default
 
-source $ANDROID_BUILD_TOP/build/envsetup.sh > /dev/null
+source $(ANDROID_BUILD_TOP)/build/envsetup.sh > /dev/null
 
-if [ ! -d "$ANDROID_PRODUCT_OUT" ]; then
-	 mkdir -p $ANDROID_PRODUCT_OUT
+if [ ! -d "$(ANDROID_PRODUCT_OUT)" ]; then
+    mkdir -p $(ANDROID_PRODUCT_OUT)
 fi
 
 gen_mkfile
 
-cd $ANDROID_PRODUCT_OUT
+cd $(ANDROID_PRODUCT_OUT)
 
 case "$1" in  
 	aboot)
-		mm aboot -j$cpus;;
+        mm aboot -j$(cpus);;
 	bootimage)
-		mm bootimage -j$cpus;;
+        mm bootimage -j$(cpus);;
 	*) 
-		echo "Android Fast Build What???\n"
+		echo -e "Android Fast Build What???\n"
 		echo "afb.sh aboot"
 		echo "afb.sh bootimage"
 		;;
