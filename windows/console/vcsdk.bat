50ee7ddf540b3cf09c9389f636dadc0a727160ea
Kidd Dawny Lu <kidd.dawny.lu@gmail.com>
2016-12-21 11:43:28 +0800

    update

--- windows/console/vcsdk.bat
+++
@@ -1,4 +0,0 @@
-@ECHO OFF
-
-::VC2010
-"C:\Program Files (x86)\Microsoft Visual Studio 10.0\VC\vcvarsall.bat" %*
---
+++ windows/wrapper/vs2015-setenv.bat
@@ -0,0 +1,4 @@
+@ECHO OFF
+
+::VC2010
+"C:\Program Files (x86)\Microsoft Visual Studio 14.0\VC\vcvarsall.bat" %*
