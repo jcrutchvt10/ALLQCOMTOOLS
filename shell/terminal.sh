22692b1d23cc368c1ca93cb15ca3e3f816603d9b
Kidd Dawny Lu <kidd.dawny.lu@gmail.com>
2017-02-16 22:35:31 +0800

    update

--- shell/terminal.sh
+++ shell/terminal.sh
@@ -4,7 +4,7 @@ if [ -f "$HOME/.winixrc" ]; then
 fi
 
 UBUNTU_IP=172.16.14.252
-UVM=10.0.1.2
+XVM=10.0.1.2
 ROUTER_IP=192.168.1.1
 
 echo "connect to $1"
@@ -12,9 +12,9 @@ case "$1" in
 	ubuntu)
 		startx.bat &
 		ssh $UBUNTU_IP -l root -p 22 -o ForwardX11Trusted=yes -o ForwardX11=yes -C -o Compression=yes;;
-	uvm)
+	xvm)
 		startx.bat &
-		ssh $UVM -p 22 -o ForwardX11Trusted=yes -o ForwardX11=yes -C -o Compression=yes;;
+		ssh $XVM -p 22 -o ForwardX11Trusted=yes -o ForwardX11=yes -C -o Compression=yes;;
 	router) ssh $ROUTER_IP -l root -p 22;;
 	*) echo "no target for $1" >&2;;
 esac
