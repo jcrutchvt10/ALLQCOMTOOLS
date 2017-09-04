27da256d82188cfffb4e49f5b9f0e1a3f9696fa1
Bouken Shounen <kidd.dawny.lu@gmail.com>
2017-09-04 13:37:48 +0800

    update
    Signed-off-by: Bouken Shounen <kidd.dawny.lu@gmail.com>

--- shell/terminal.sh
+++ shell/terminal.sh
@@ -1,20 +1,19 @@
 #!/bin/bash
+
 if [ -f "$HOME/.winixrc" ]; then
 	source ~/.winixrc
 fi
 
-UBUNTU_IP=172.16.14.252
 XVM=10.0.1.129
 ROUTER_IP=192.168.1.1
+WORK=user5@10.150.20.15
 
 echo "connect to $1"
 case "$1" in  
-	ubuntu)
-		startx.bat &
-		ssh $UBUNTU_IP -p 22 -o ForwardX11Trusted=yes -o ForwardX11=yes -C -o Compression=yes;;
 	xvm)
 		startx.bat &
 		ssh $XVM -p 22 -o ForwardX11Trusted=yes -o ForwardX11=yes -C -o Compression=yes;;
 	router) ssh $ROUTER_IP -l root -p 22;;
+	work) ssh $WORK -p 22 -o ForwardX11Trusted=yes -o ForwardX11=yes -C -o Compression=yes;;
 	*) echo "no target for $1" >&2;;
 esac
