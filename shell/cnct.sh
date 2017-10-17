49f6cf4fa7df3aa34f8e30313d893b8c09c1af0b
kiddlu <kidd.dawny.lu@gmail.com>
2017-10-17 21:32:03 +0800

    update
    Signed-off-by: kiddlu <kidd.dawny.lu@gmail.com>

--- shell/cnct.sh
+++ shell/cnct.sh
@@ -4,15 +4,13 @@ if [ -f "$HOME/.winixrc" ]; then
 	source ~/.winixrc
 fi
 
-XVM=10.0.1.129
+CLOUD=root@139.162.115.228
 ROUTER_IP=192.168.1.1
 WORK=user5@10.150.20.15
 
 echo "connect to $1"
 case "$1" in  
-	xvm)
-		startx.bat &
-		ssh $XVM -p 22 -o ForwardX11Trusted=yes -o ForwardX11=yes -C -o Compression=yes;;
+	cloud)  ssh $CLOUD -p 22 -o ForwardX11Trusted=yes -o ForwardX11=yes -C -o Compression=yes;;
 	router) ssh $ROUTER_IP -l root -p 22;;
 	work) ssh $WORK -p 22 -o ForwardX11Trusted=yes -o ForwardX11=yes -C -o Compression=yes;;
 	*) echo "no target for $1" >&2;;
